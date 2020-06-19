c655e1465bee0539464691551700fc7f4f0c09f2
Eliecer <eliecer2000@gmail.com>
2020-06-18 19:41:40 -0500

    Iniciando Repositorio

---
+++ README.md
@@ -0,0 +1,13 @@
+# vscode-WhatTheCommit
+
+[![CircleCI](https://circleci.com/gh/Gaardsholt/vscode-whatthecommit.svg?style=svg)](https://circleci.com/gh/Gaardsholt/vscode-whatthecommit)
+
+This extension will fetch your next commit message from WhatTheCommit.com
+
+All credits regarding the commit messages goes to https://github.com/ngerakines/commitment
+
+![alt text](resources/images/demo.gif "Extension demo")
+
+## Download
+Can be downloaded from [Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=Gaardsholt.vscode-whatthecommit)
+"# vscode-message-commit-time" 
---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import * as vscode from "vscode";
+import * as http from "http";
+
+import moment from "moment";
+
+export async function activate(context: vscode.ExtensionContext) {
+  console.log('Successfully activated "WhatTheCommit"');
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.wtc.getCommitMessage",
+      async () => {
+        const gitExtension = vscode.extensions.getExtension("vscode.git")
+          .exports;
+        const repo = gitExtension.getAPI(1).repositories[0];
+
+        // http
+        //   .get("http://www.whatthecommit.com/index.json", (resp) => {
+        //     let data = "";
+        //     resp.on("data", (chunk) => {
+        //       data += chunk;
+        //     });
+        //     resp.on("end", () => {
+        //       let myData = JSON.parse(data);
+        //       repo.inputBox.value = myData.commit_message;
+        //       // vscode.window.showInformationMessage(myData.commit_message);
+        //     });
+        //   })
+        //   .on("error", (err) => {
+        //     vscode.window.showErrorMessage(
+        //       "Unable to connect to whatthecommit.com: " + err.message,
+        //       { modal: true }
+        //     );
+        //   });
+      }
+    )
+  );
+}
